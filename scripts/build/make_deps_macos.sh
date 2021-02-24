f6a9fa7c17e1ff0f1a476d6e7111eb479a8f6206
Abhishek Dutta <thelinuxmaniac@gmail.com>
2021-02-24 10:47:19 +0000

    updated dependency build scripts to show message on failure

--- scripts/build/make_deps_macos.sh
+++ scripts/build/make_deps_macos.sh
@@ -53,5 +53,39 @@ if ! [ -d "${DEPDIR}/include/vl" ]; then
   cd $DEPSRC && wget http://www.vlfeat.org/download/vlfeat-0.9.21-bin.tar.gz && tar -zxvf vlfeat-0.9.21-bin.tar.gz && cd vlfeat-0.9.21 && make -j16 && cp "${DEPSRC}/vlfeat-0.9.21/bin/maci64/libvl.dylib" "${DEPDIR}/lib/libvl.dylib" && mkdir "${DEPDIR}/include/vl" && cp -fr $DEPSRC/vlfeat-0.9.21/vl/*.* "${DEPDIR}/include/vl/"
 fi
 
-echo "****************************************************************"
-echo "All dependencies downloaded, compiled and installed to ${DEPDIR}"
+## check if all the required dependencies are installed correctly
+echo "***********************************************"
+all_is_well=true
+if ! [ -f "${DEPDIR}/bin/cmake" ]; then
+    echo "failed to install cmake build tool which is used to compile the VISE software."
+    all_is_well=false
+fi
+if ! [ -d "${DEPDIR}/include/boost" ]; then
+    echo "failed to install boost library which is used to create http web server used by VISE."
+    all_is_well=false
+fi
+if ! [ -d "${DEPDIR}/include/ImageMagick-6" ]; then
+    echo "failed to install imagemagick library which is used to load and save images."
+    all_is_well=false
+fi
+if ! [ -d "${DEPDIR}/include/google/protobuf" ]; then
+    echo "failed to install protobuf library which is required to store search index files."
+    all_is_well=false
+fi
+if ! [ -d "${DEPDIR}/include/eigen3/Eigen" ]; then
+    echo "failed to install eigen library which powers the image comparison tool."
+    all_is_well=false
+fi
+if ! [ -d "${DEPDIR}/include/vl" ]; then
+    echo "failed to install vlfeat library which is used to extract features from images."
+    all_is_well=false
+fi
+if ! [ -f "${DEPDIR}/include/sqlite3.h" ]; then
+    echo "failed to install sqlite database library which is used to store metadata."
+    all_is_well=false
+fi
+
+if [ "$all_is_well" = true ] ; then
+    echo "Dependencies downloaded, compiled and installed to ${DEPDIR}"
+    echo "You can now proceed to compiling the VISE software."
+fi
