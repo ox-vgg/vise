4c9ccdb33f91da98ea3a999e822a4825c9607b7a
Abhishek Dutta <thelinuxmaniac@gmail.com>
2021-06-25 04:16:33 +0100

    downgraded boost dependency from 1.76 to 1.73 as boost.asio introduces breaking API changes in new version

--- scripts/build/make_deps_debian.sh
+++ scripts/build/make_deps_debian.sh
@@ -34,7 +34,7 @@ fi
 
 ## boost
 if ! [ -d "${DEPDIR}/include/boost" ]; then
-  cd $DEPSRC && wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz && tar -zxvf boost_1_76_0.tar.gz && cd boost_1_76_0 && ./bootstrap.sh --prefix=$DEPDIR --with-toolset=gcc --with-libraries=filesystem,system,thread && ./b2 --with-filesystem --with-system --with-thread variant=release threading=multi toolset=gcc install
+  cd $DEPSRC && wget https://boostorg.jfrog.io/artifactory/main/release/1.73.0/source/boost_1_73_0.tar.gz && tar -zxvf boost_1_73_0.tar.gz && cd boost_1_73_0 && ./bootstrap.sh --prefix=$DEPDIR --with-toolset=gcc --with-libraries=filesystem,system,thread && ./b2 --with-filesystem --with-system --with-thread variant=release threading=multi toolset=gcc install
 fi
 
 # imagemagick
